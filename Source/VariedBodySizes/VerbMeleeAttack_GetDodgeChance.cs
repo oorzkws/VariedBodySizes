638a9b111750570689786a9276df900dce0542eb
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-11-06 22:35:03 -0700

    Body-size based dodge chance

---
+++ Source/VariedBodySizes/VerbMeleeAttack_GetDodgeChance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VariedBodySizes;
+
+[HarmonyPatch(typeof(Verb_MeleeAttack), "GetDodgeChance")]
+public static class VerbMeleeAttack_GetDodgeChance
+{
+    public static void Postfix(ref float __result, LocalTargetInfo target)
+    {
+        
+        if (!VariedBodySizesMod.instance.Settings.AffectMeleeDodgeChance)
+        {
+            return;
+        }
+
+        if (target.Thing is not Pawn pawn)
+            return;
+        
+        __result /= Main.CurrentComponent.GetVariedBodySize(pawn);
+    }
+}
