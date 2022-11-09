f1491d4f4f5ed64877abf31b4a14b7967116307b
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-11-09 01:15:49 -0700

    Vanilla Expanded Framework compat

--- Source/VariedBodySizes/GraphicMeshSet_MeshAt.cs
+++ Source/VariedBodySizes/GraphicMeshSet_MeshAt.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Reflection;
 using HarmonyLib;
 using UnityEngine;
 using Verse;
@@ -20,5 +22,59 @@ public static class GraphicMeshSet_MeshAt
         }
 
         __result = Main.GetPawnMesh(Main.CurrentComponent.GetVariedBodySize(Main.CurrentPawn), rot.AsInt == 3);
+    }
+}
+
+// VE breaks pawn render scaling with transpiler fun times so we have to modify the meshes that get passed to it...
+[HarmonyPatch]
+public static class VEF_DrawSettings_TryGetNewMeshPatch
+{
+    private static readonly string[] VEFMethods = new[]
+    {
+        "VFECore.Patch_PawnRenderer_DrawPawnBody_Transpiler:ModifyMesh",
+        "VFECore.Patch_DrawHeadHair_DrawApparel_Transpiler:TryModifyMeshRef",
+        "VFECore.Harmony_PawnRenderer_DrawBodyApparel:ModifyShellMesh",
+        "VFECore.Harmony_PawnRenderer_DrawBodyApparel:ModifyPackMesh"
+    };
+
+    public static bool Prepare()
+    {
+        foreach (var methodPath in VEFMethods)
+        {
+            var targetMethod = AccessTools.Method(methodPath);
+            if (targetMethod == null)
+            {
+                Log.Warning("VEF not found");
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static IEnumerable<MethodBase> TargetMethods()
+    {
+        foreach (var methodPath in VEFMethods)
+        {
+            var targetMethod = AccessTools.Method(methodPath);
+            if (targetMethod == null)
+                Log.Warning("targetMethod not found");
+            else
+                yield return targetMethod;
+        }
+    }
+
+    public static void Prefix(ref Pawn pawn, ref Mesh mesh)
+    {
+        if (Main.CurrentPawn == null)
+        {
+            return;
+        }
+
+        if (Main.CurrentComponent == null)
+        {
+            return;
+        }
+
+        mesh = Main.GetPawnMesh(Main.CurrentComponent.GetVariedBodySize(pawn), pawn.Rotation.AsInt == 3);
     }
 }
