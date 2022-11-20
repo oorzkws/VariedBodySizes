2818caa6e25ecf0e31a2ae17b7c0c1eafe370e4a
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-11-20 08:17:52 -0700

    VEF patch removal for now

--- Source/VariedBodySizes/Main.cs
+++ Source/VariedBodySizes/Main.cs
@@ -22,30 +22,23 @@ public static class Main
         HarmonyInstance = new Harmony("Mlie.VariedBodySizes");
         HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
         VariedBodySizesMod.instance.Settings.VariedBodySizes ??= new Dictionary<string, FloatRange>();
+        // Until VEF works we're just going to override with our own scaling.
         foreach (var targetPair in new KeyValuePair<Type, string>[]{
             new(typeof(HumanlikeMeshPoolUtility),"GetHumanlikeBodySetForPawn"),
             new(typeof(HumanlikeMeshPoolUtility),"GetHumanlikeHeadSetForPawn"),
             new(typeof(HumanlikeMeshPoolUtility),"GetHumanlikeHairSetForPawn"),
             new(typeof(HumanlikeMeshPoolUtility),"GetHumanlikeBeardSetForPawn"),
             new(typeof(PawnRenderer),"GetBodyOverlayMeshSet"),
-            new(typeof(PawnRenderer),"BaseHeadOffsetAt")
+            new(typeof(PawnRenderer),"BaseHeadOffsetAt"),
+            new(AccessTools.TypeByName("Verse.PawnRenderer+<>c__DisplayClass54_0"), "<DrawHeadHair>g__DrawExtraEyeGraphic|6")
         })
         {
             var targetMethod = AccessTools.Method(targetPair.Key, targetPair.Value);
-            if (targetMethod != null)
-            {
-                var patches = Harmony.GetPatchInfo(targetMethod);
-                if (patches is null)
-                {
-                    continue;
-                }
-                if (!patches.Owners.Contains("OskarPotocki.VFECore"))
-                {
-                    continue;
-                }
-                //FileLog.Log($"Unpatching {targetMethod.DeclaringType?.Name ?? string.Empty}:{targetMethod.Name}");
-                //HarmonyInstance.Unpatch(targetMethod, HarmonyPatchType.All, "OskarPotocki.VFECore");
-            }
+            if (targetMethod == null) continue;
+            var patches = Harmony.GetPatchInfo(targetMethod);
+            if (patches?.Owners.Contains("OskarPotocki.VFECore") != true) continue;
+            LogMessage($"Unpatching {targetMethod.DeclaringType?.Name ?? string.Empty}:{targetMethod.Name}", true);
+            HarmonyInstance.Unpatch(targetMethod, HarmonyPatchType.All, "OskarPotocki.VFECore");
         }
     }
 
