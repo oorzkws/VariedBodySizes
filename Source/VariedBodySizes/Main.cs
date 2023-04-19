dd31458ee6934e7b6914b88d98b2b9397e84421b
oorzkws <65210810+oorzkws@users.noreply.github.com>
2023-04-19 06:34:01 -0600

    New helper functions
    Allows us to write transpilers as regular C#, if the code is simple enough

--- Source/VariedBodySizes/Main.cs
+++ Source/VariedBodySizes/Main.cs
@@ -128,16 +128,94 @@ public static partial class HarmonyPatches
     }
 
     // CodeMatcher will throw errors if we try to take actions in an invalid state (i.e. no match)
-    private static void OnSuccess(CodeMatcher match, Action<CodeMatcher> action)
+    private static CodeMatcher OnSuccess(this CodeMatcher match, Action<CodeMatcher> action, bool suppress = false)
     {
-        if (match.IsInvalid)
+        switch (match.IsInvalid)
         {
-            Main.LogMessage(
-                $"Transpiler did not find target @ {new StackTrace().GetFrame(1).GetMethod().DeclaringType?.FullName ?? "unknown"}",
-                true);
-            return;
+            case true when !suppress: 
+                Main.LogMessage($"Transpiler did not find target @ {new StackTrace().GetFrame(1).GetMethod().DeclaringType?.FullName ?? "unknown"}", true);
+                break;
+            case false:
+                action.Invoke(match);
+                break;
         }
 
-        action.Invoke(match);
+        return match;
+    }
+
+    /// <summary>
+    /// Replaces the pattern with the replacement. This is a naive implementation - if you need labels, DIY it.
+    /// </summary>
+    /// <param name="match">the CodeMatcher instance to use</param>
+    /// <param name="pattern">instructions to match, the beginning of this match is where the replacement begins</param>
+    /// <param name="replacement">the new instructions</param>
+    /// <param name="replace">Whether we should keep labels and set the opcodes/operands instead of recreating the CodeInstruction</param>
+    /// <param name="suppress">Whether to suppress the log message on a failed match</param>
+    /// <returns></returns>
+    private static CodeMatcher Replace(this CodeMatcher match, CodeMatch[] pattern, CodeInstructions replacement, bool replace = true, bool suppress = false)
+    {
+        return match.MatchStartForward(pattern).OnSuccess(matcher =>
+        {
+            var newOps = replacement.ToList();
+            for (var i = 0; i < Math.Max(newOps.Count, pattern.Length); i++)
+            {
+                if (i < newOps.Count)
+                {
+                    var op = newOps[i];
+                    if (i < pattern.Length)
+                    {
+                        if (replace)
+                        {
+                            // This keeps labels
+                            matcher.SetAndAdvance(op.opcode, op.operand);
+                        }
+                        else
+                        {
+                            matcher.RemoveInstruction();
+                            matcher.InsertAndAdvance(op);
+                        }
+                    }
+                    else
+                    {
+                        matcher.InsertAndAdvance(op);
+                    }
+                }
+                else
+                {
+                    matcher.RemoveInstruction();
+                }
+            }
+        }, suppress);
+    }
+
+    /// <summary>
+    /// Returns the given method as a basic ILCode signature, excluding ret statements
+    /// </summary>
+    /// <param name="method">The method to convert</param>
+    /// <returns>A set of CodeInstructions representing the method given</returns>
+    private static CodeInstructions InstructionSignature(Delegate method)
+    {
+        // arg indexes get shifted by 1 if we use this method, so we have to shift them back
+        return PatchProcessor.GetCurrentInstructions(method.Method)
+            .Manipulator(
+                i => i.IsLdarg() || i.IsStarg(),
+                i =>
+                {
+                    var index = new FishInstruction(i).GetIndex() - 1;
+                    var copy = i.IsLdarg() ? FishTranspiler.Argument(index) : FishTranspiler.StoreArgument(index);
+                    // Replace the opcode and operand with the proper one
+                    i.With(copy.OpCode, copy.Operand);
+                })
+            .Where(i => i.opcode != OpCodes.Ret && i.opcode != OpCodes.Nop);
+    }
+
+    /// <summary>
+    /// Returns the given method as a basic ILCode signature wrapped in a CodeMatch, excluding ret statements
+    /// </summary>
+    /// <param name="method">The method to convert</param>
+    /// <returns>A set of CodeInstructions representing the method given</returns>
+    private static CodeMatch[] InstructionMatchSignature(Delegate method)
+    {
+        return InstructionSignature(method).Select(i => new CodeMatch(i.opcode, i.operand)).ToArray();
     }
 }
