6827f99e28270f23a68acf532fdd808477cdd598
Mlie <emipa606@users.noreply.github.com>
2024-11-02 19:34:37 +0100

    Removed the outdated VE-patch

--- Source/VariedBodySizes/HarmonyPatches/Compat/VeFrameworkPatches.cs
+++ Source/VariedBodySizes/HarmonyPatches/Compat/VeFrameworkPatches.cs
@@ -1,37 +1,38 @@
-namespace VariedBodySizes;
+//namespace VariedBodySizes;
 
-public static partial class HarmonyPatches
-{
-    // We're just going to patch ourselves if it's relevant
-    [HarmonyPatch]
-    public static class VeFramework_OnCalculateBodySizePatch
-    {
-        private static readonly BodyScaleDelegate vefBodyScaleMethod = hasVef
-            ? AccessTools.MethodDelegate<BodyScaleDelegate>(
-                "VanillaGenesExpanded.HumanlikeMeshPoolUtility_Patches:GeneScaleFactor")
-            : null;
-
-
-        public static bool Prepare()
-        {
-            return hasVef && NotNull(vefBodyScaleMethod);
-        }
+//public static partial class HarmonyPatches
+//{
+//    // We're just going to patch ourselves if it's relevant
+//    [HarmonyPatch]
+//    public static class VeFramework_OnCalculateBodySizePatch
+//    {
+//        private static readonly BodyScaleDelegate vefBodyScaleMethod = hasVef
+//            ? AccessTools.MethodDelegate<BodyScaleDelegate>(
+//                "VanillaGenesExpanded.HumanlikeMeshPoolUtility_Patches:GeneScaleFactor")
+//            : null;
 
 
-        public static MethodBase TargetMethod()
-        {
-            return AccessTools.Method("VariedBodySizes.VariedBodySizes_GameComponent:OnCalculateBodySize");
-        }
+//        public static bool Prepare()
+//        {
+//            return hasVef && NotNull(vefBodyScaleMethod);
+//        }
 
 
-        public static void Postfix(ref float __result, Pawn pawn)
-        {
-            if (pawn is not null)
-            {
-                __result = vefBodyScaleMethod(__result, pawn);
-            }
-        }
+//        public static MethodBase TargetMethod()
+//        {
+//            return AccessTools.Method("VariedBodySizes.VariedBodySizes_GameComponent:OnCalculateBodySize");
+//        }
 
-        private delegate float BodyScaleDelegate(float width, Pawn pawn);
-    }
-}
+
+//        public static void Postfix(ref float __result, Pawn pawn)
+//        {
+//            if (pawn is not null)
+//            {
+//                __result = vefBodyScaleMethod(__result, pawn);
+//            }
+//        }
+
+//        private delegate float BodyScaleDelegate(float width, Pawn pawn);
+//    }
+//}
+
